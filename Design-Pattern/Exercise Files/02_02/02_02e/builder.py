90da8a9825df8cdce97e5a3e7a7925a18c18534d
Duc Do <ducdh1210@gmail.com>
2024-08-08 08:19:25 -0500

    formatted scripts

--- Design-Pattern/Exercise Files/02_02/02_02e/builder.py
+++ Design-Pattern/Exercise Files/02_02/02_02e/builder.py
@@ -1,32 +1,32 @@
-class Director():
+class Director:
     """Director"""
+
     def __init__(self, builder):
-        self._builder = builder 
-        
+        self._builder = builder
+
     def construct_car(self):
         self._builder.create_new_car()
         self._builder.add_model()
         self._builder.add_tires()
         self._builder.add_engine()
-        
+
     def get_car(self):
         return self._builder.car
-        
-        
- 
-class Builder():
+
+
+class Builder:
     """Abstract Builder"""
+
     def __init__(self):
-        self.car = None 
-        
+        self.car = None
+
     def create_new_car(self):
         self.car = Car()
-        
 
 
 class SkyLarkBuilder(Builder):
-    """Concrete Builder --> provides parts and tools to work on the parts """
-    
+    """Concrete Builder --> provides parts and tools to work on the parts"""
+
     def add_model(self):
         self.car.model = "Skylark"
 
@@ -37,15 +37,17 @@ class SkyLarkBuilder(Builder):
         self.car.engine = "Turbo engine"
 
 
-class Car():
+class Car:
     """Product"""
+
     def __init__(self):
         self.model = None
         self.tires = None
         self.engine = None
-        
+
     def __str__(self):
-        return '{} | {} | {}'.format(self.model, self.tires, self.engine)
+        return "{} | {} | {}".format(self.model, self.tires, self.engine)
+
 
 builder = SkyLarkBuilder()
 director = Director(builder)
