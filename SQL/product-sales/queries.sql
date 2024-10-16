cb1da108dc1d1c4a061a4a61690514525a46c1f1
Duc Do <ducdh1210@gmail.com>
2024-10-16 09:32:05 -0500

    sql quetsions

--- SQL/product-sales/queries.sql
+++ SQL/product-sales/queries.sql
@@ -13,4 +13,14 @@ SELECT strftime('%Y-%m', sale_date) as month, SUM(quantity) as monthly_quantity
 FROM sales
 GROUP BY strftime('%Y-%m', sale_date);
 
-
+-- Query 4: Product sales quantity by quarter
+SELECT product_id, product_name,
+       CASE 
+           WHEN strftime('%m', sale_date) IN ('01','02','03') THEN 'Q1'
+           WHEN strftime('%m', sale_date) IN ('04','05','06') THEN 'Q2'
+           WHEN strftime('%m', sale_date) IN ('07','08','09') THEN 'Q3'
+           ELSE 'Q4'
+       END as quarter,
+       SUM(quantity) as quarterly_quantity
+FROM sales
+GROUP BY product_id, product_name, quarter;
